3521bd1dbbb94ca591595a574365215c53840575
Victor Douet <victor.douet@gmail.com>
2021-07-07 18:01:23 -0400

    Modified license

--- OpenShoe C Realtime/zero_velocity_detector.c
+++ OpenShoe C Realtime/zero_velocity_detector.c
@@ -1,6 +1,8 @@
 /*
 Modified work Copyright (c) 2020 Victor Douet <victor.douet@gmail.com>, ISC License (open source)
 Original work: Copyright (c) 2011 OpenShoe, ISC License (open source)
+
+Change: Translated original Matlab implementation in C and convert it to real-time
 */
 
 #include "zero_velocity_detector.h"
